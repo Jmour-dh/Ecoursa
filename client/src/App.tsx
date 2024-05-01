d34c6b5dd912853add2b878f062e7da5cbb2b150
Dhia Eddine Jmour <125797700+Jmour-dh@users.noreply.github.com>
2024-05-01 13:25:00 +0200

    01-05-2024-13h24-deocker-prod

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR hhhh
+          Edit <code>src/App.tsx</code> and save to test HMR hhhh dhia eee zeee oooo kkkk 
         </p>
       </div>
       <p className="read-the-docs">
